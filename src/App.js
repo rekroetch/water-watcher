e74ea205cec54f6ba9d65ce15b74fca6a5f81b3a
Rachel Kroetch <rekroetch@ucdavis.edu>
2021-06-07 16:46:53 -0700

    onClick added to count water

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 import "./app.css";
 
+
 function App() {
+  const [water, setWater] = useState(0)
+
+  function addWater(event) {
+    event.preventDefault()
+    setWater(water+1)
+  }
+
   return (
     <div className="container"> 
       <header className="title">Water Watcher</header>
       <div className="howMuch">
-        <div>How much water did you drink today?</div>
-        <div>Goal: ___</div>
+        <div className="prompt">Click for every cup of water you drank today!</div>
+        <div onClick={addWater} value="1"><i className="fas fa-tint droplet"></i></div>
+        <div className="totalWater">{water} cup(s)</div>
       </div>
     </div>
   );
