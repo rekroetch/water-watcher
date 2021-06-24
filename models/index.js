147f072bdfe38cb8fcf88e462c44b3e26984391e
Rachel Kroetch <rekroetch@ucdavis.edu>
2021-06-24 12:01:10 -0700

    change schema name to Water

--- models/index.js
+++ models/index.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
+const waterSchema = new Schema({
   date: { type: Date, default: Date.now },
   water: { type: Number, required: true },
 });
 
-const User = mongoose.model("user", userSchema);
+const Water = mongoose.model("water", waterSchema);
 
-module.exports = User;
+module.exports = Water;
